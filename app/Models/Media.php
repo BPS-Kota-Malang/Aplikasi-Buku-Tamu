42eeee60f56733fd95507225aac2c8a2f73959e1
darman1725 <darmansaragih1701@gmail.com>
2022-07-20 13:08:35 +0700

    Menambahkan tabel media

---
+++ app/Models/Media.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Media extends Model
+{
+    use HasFactory;
+    protected $guarded=['id'];
+    protected $table = "media";
+    protected $fillable = ['media_type'];
+}
