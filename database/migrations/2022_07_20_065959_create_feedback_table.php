0796336840f56165bd7b652c06bc6cef743bd3d7
wildan220 <wildanachmad73@gmail.com>
2022-07-20 14:59:39 +0700

    membuat feedback

---
+++ database/migrations/2022_07_20_065959_create_feedback_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feedback', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_transaction');
+            $table->foreign('id_transaction')->references('id')->on('transaction');
+            $table->int('rating_service');
+            $table->int('rating_facilities');
+            $table->int('rating_dataquality');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feedback');
+    }
+};
