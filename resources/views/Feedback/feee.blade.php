f73ba20040e612175ddf4cd559ac9752577b5fd0
wildan220 <wildanachmad73@gmail.com>
2022-07-22 15:32:59 +0700

    tambahan feedback

--- resources/views/Feedback/feee.blade.php
+++ resources/views/Feedback/feee.blade.php
@@ -8,11 +8,65 @@
   </head>
   <body>
     <div class="container">
+      <div class="next">
+        <div class="text">Pelayanan</div>
+        <div class="neext">NEXT</div>
+      </div>
+      <div class="star-pelayanan">
+        <input type="radio" name="rate" id="rate-5">
+        <label for="rate-5" class="fas fa-star"></label>
+        <input type="radio" name="rate" id="rate-4">
+        <label for="rate-4" class="fas fa-star"></label>
+        <input type="radio" name="rate" id="rate-3">
+        <label for="rate-3" class="fas fa-star"></label>
+        <input type="radio" name="rate" id="rate-2">
+        <label for="rate-2" class="fas fa-star"></label>
+        <input type="radio" name="rate" id="rate-1">
+        <label for="rate-1" class="fas fa-star"></label>
+        {{-- <form action="#">
+          <header></header>
+          <div class="textarea">
+            <textarea cols="30" placeholder="kritik dan saran"></textarea>
+          </div>
+          <div class="btn">
+            <button type="submit">Next</button>
+          </div>
+        </form> --}}
+      </div>
+    </div>
+    <div class="container">
+      <div class="next">
+        <div class="text">Sarana dan Prasarana</div>
+        <div class="neext">NEXT</div>
+      </div>
+      <div class="star-sarana">
+        <input type="radio" name="rate" id="rate-5">
+        <label for="rate-5" class="fas fa-star"></label>
+        <input type="radio" name="rate" id="rate-4">
+        <label for="rate-4" class="fas fa-star"></label>
+        <input type="radio" name="rate" id="rate-3">
+        <label for="rate-3" class="fas fa-star"></label>
+        <input type="radio" name="rate" id="rate-2">
+        <label for="rate-2" class="fas fa-star"></label>
+        <input type="radio" name="rate" id="rate-1">
+        <label for="rate-1" class="fas fa-star"></label>
+        {{-- <form action="#">
+          <header></header>
+          <div class="textarea">
+            <textarea cols="30" placeholder="kritik dan saran"></textarea>
+          </div>
+          <div class="btn">
+            <button type="submit">Next</button>
+          </div>
+        </form> --}}
+      </div>
+    </div>
+    <div class="container">
       <div class="post">
-        <div class="text">Thanks for rating us!</div>
+        <div class="text">Kualitas Data</div>
         <div class="edit">EDIT</div>
       </div>
-      <div class="star-widget">
+      <div class="star-kualitas">
         <input type="radio" name="rate" id="rate-5">
         <label for="rate-5" class="fas fa-star"></label>
         <input type="radio" name="rate" id="rate-4">
@@ -26,7 +80,7 @@
         <form action="#">
           <header></header>
           <div class="textarea">
-            <textarea cols="30" placeholder="Describe your experience.."></textarea>
+            <textarea cols="30" placeholder="kritik dan saran"></textarea>
           </div>
           <div class="btn">
             <button type="submit">Post</button>
@@ -37,13 +91,33 @@
     <script>
       const btn = document.querySelector("button");
       const post = document.querySelector(".post");
-      const widget = document.querySelector(".star-widget");
+      const widget = document.querySelector(".star-pelayanan");
+      const widget2 = document.querySelector(".star-sarana");
+      const widget 3= document.querySelector(".star-kualitas");
       const editBtn = document.querySelector(".edit");
       btn.onclick = ()=>{
         widget.style.display = "none";
         post.style.display = "block";
         editBtn.onclick = ()=>{
           widget.style.display = "block";
+          post.style.display = "none";
+        }
+        return false;
+      }
+      btn.onclick = ()=>{
+        widget1.style.display = "none";
+        post.style.display = "block";
+        editBtn.onclick = ()=>{
+          widget1.style.display = "block";
+          post.style.display = "none";
+        }
+        return false;
+      }
+      btn.onclick = ()=>{
+        widge2t.style.display = "none";
+        post.style.display = "block";
+        editBtn.onclick = ()=>{
+          widget2.style.display = "block";
           post.style.display = "none";
         }
         return false;
