d793f1fedf3b65cdb8502a11a23c27ed0b9afcc2
darman1725 <darmansaragih1701@gmail.com>
2022-07-27 12:29:18 +0700

    Menambahkan Library DomPDF dan mengaplikasikannya

--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -209,7 +210,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // 'ExampleClass' => App\Example\ExampleClass::class,
+         'PDF' => \Barryvdh\DomPDF\Facade::class,
     ])->toArray(),
 
 ];
