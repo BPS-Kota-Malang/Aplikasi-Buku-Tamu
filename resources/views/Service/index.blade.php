3a055315fdf51f83c691f4abdcad177d04e82945
darman1725 <darmansaragih1701@gmail.com>
2022-07-27 12:43:22 +0700

    Melakukan modifikasi script css dan js pada file index

--- resources/views/Service/index.blade.php
+++ resources/views/Service/index.blade.php
@@ -55,14 +55,13 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.print.min.js"></script>
 
 <script>
     $(document).ready( function () {
     $('#myTable').DataTable({
-        dom: 'Bfrtip',
-        scrollX: true
+        dom: 'Bfrtip'
     });
     });
 </script>
