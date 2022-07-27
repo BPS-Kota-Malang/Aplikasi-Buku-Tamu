d4ef6f2ddbe1f29cf677590d702b5262fbc1a1c6
darman1725 <darmansaragih1701@gmail.com>
2022-07-27 08:44:24 +0700

    Menampilkan Data List Admin

---
+++ resources/views/Auth/index.blade.php
@@ -0,0 +1,71 @@
+@extends('app')
+@section('content')
+<div class="card">
+    @if (session('status'))
+    <div class="alert alert-success">
+        {{ session('status') }}
+    </div>
+    @endif
+    <div class="card-header">
+        Data Admin <br><br>
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+        <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.5.6/css/buttons.dataTables.min.css">
+
+    </div>
+    <div class="card-body">
+        <table class="table" id="myTable">
+            <thead class="thead-dark text-center">
+                <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Nama Admin</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($pengguna as $item)
+                <tr>
+                    <td>{{ $loop->iteration}}
+                    <td>{{ $item->name}}
+                    <td>{{ $item->email}}
+                    <td>
+                        <div class="row">
+                                <div class="col-4">
+                                    <form action="{{route('list.destroy', $item->id)}}" method="post">
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" class="btn btn-danger"
+                                            onclick="return confirm('Apakah anda yakin ingin menghapus Data Admin ini ?')">Delete</button>
+                                    </form>
+                                </div>
+                        </div>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+@endsection
+
+@section('script')
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.flash.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.print.min.js"></script>
+
+<script>
+    $(document).ready( function () {
+    $('#myTable').DataTable({
+        dom: 'Bfrtip',
+        buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
+    });
+    });
+</script>
+@endsection
