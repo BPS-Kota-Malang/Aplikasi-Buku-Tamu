5e97176128bdaf062f01fc04e411769341587f43
darman1725 <darmansaragih1701@gmail.com>
2022-07-28 08:28:10 +0700

    Membuat migrasi purpose dan mengimplementasikan CRUD

---
+++ resources/views/Purpose/detailForm.blade.php
@@ -0,0 +1,22 @@
+@extends('app')
+@section('content')
+<div class="col-sm-12">
+    <div class="card">
+        <div class="card-body">
+        <h4><i class="fa fa-pencil-alt"></i> Detail Jenis Tujuan</h4><br>
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <td>Jenis Tujuan</td>
+                        <td>:</td>
+                        <td>{{ $purpose->purpose_type }}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <div class="button">
+                <a href="{{ route('purpose.index')}}" class="btn btn-primary">Kembali</a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/Purpose/index.blade.php
@@ -0,0 +1,69 @@
+@extends('app')
+@section('content')
+<div class="card">
+    @if (session('status'))
+    <div class="alert alert-success">
+        {{ session('status') }}
+    </div>
+    @endif
+    <div class="card-header">
+        <h5 class="mb-0 text-center">Data Tujuan</h5>
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+    </div>
+    <div class="card-body">
+        <a href="{{route('purpose.create')}}" class="btn btn-success">Add Data</a>
+        <table class="table" id="myTable">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Jenis Tujuan</th>
+                    <th scope="col" width="20%">Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($purpose as $item)
+                <tr>
+
+                    <td>{{ $loop->iteration}}
+                    <td>{{ $item->purpose_type}}
+                    <td>
+                        <div class="row">
+                                <div class="col-4">
+                                    <a href="{{ route('purpose.edit', $item->id)}}" class="btn btn-warning">Edit</a>
+                                </div>
+                                <div class="col-4">
+                                    <a href="{{ route('purpose.show', $item->id)}}" class="btn btn-info">Show</a>
+                                </div>
+                                <div class="col-4">
+                                    <form action="{{route('purpose.destroy', $item->id)}}" method="post">
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" class="btn btn-danger"
+                                            onclick="return confirm('Apakah anda yakin ingin menghapus Jenis Tujuan ini ?')">Delete</button>
+                                    </form>
+                                </div>
+                        </div>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+@endsection
+
+@section('script')
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.print.min.js"></script>
+
+<script>
+    $(document).ready( function () {
+    $('#myTable').DataTable({
+        dom: 'Bfrtip'
+    });
+    });
+</script>
+@endsection
