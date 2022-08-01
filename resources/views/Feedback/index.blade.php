e27da3562862be6426d4ae00f28d06a0e627e0be
unknown <bimasakti.kr@gmail.com>
2022-08-01 14:00:08 +0700

    get transactionID on Feedback

--- resources/views/Feedback/index.blade.php
+++ resources/views/Feedback/index.blade.php
@@ -7,7 +7,9 @@
     <title>Feedback</title>
     <h1 class="mb-3 text-center">Feedback </h1>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-</head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  </head>
 
 <body>
     <div class="form-step form-step-active">
@@ -31,13 +33,17 @@
                 <td>{{ $tran->data}}</td>
                 <td>
                 <!-- Button trigger modal -->
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                  feedback
+                <button type="button" class="btn btn-sm feedbackBtn btn-primary" data-toggle="modal"data-target="">
+                  Update
                 </button>
+              </td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
 
                 <!-- Modal -->
-                <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                  <div class="modal-dialog">
+                <div class="modal fade" id="feedbackModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">                  <div class="modal-dialog">
                     <div class="modal-content">
                       <div class="modal-header">
                         <h5 class="modal-title" id="staticBackdropLabel">feedback</h5>
@@ -46,8 +52,11 @@
                       <div class="modal-body">
                         <form method="post" action="feedback"  class="sigin-form">
                           @csrf
-
+                        
                         <col-md-10>
+                        <div class="col-md-8">
+                          <h6 id="transactionID"></h6>
+                        </div>
                         <div class="col-md-8">
                           <label>pelayanan</label>
                         </div>
@@ -135,17 +144,29 @@
 
                 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
-                </td>
-              </tr>
-              @endforeach
+              
 
-            </tbody>
-          </table>
+           
 
           <!-- Button trigger modal -->
 
-
-
+          <script>
+            $(document).ready(function(){
+                $('.feedbackBtn').on('click', function(){
+        
+                    $('#feedbackModal').modal('show');
+                    $tr =$(this).closest('tr');
+                    var data= $tr.children("td").map(function(){
+                      return $(this).text();
+                    }).get();
+        
+                    
+                    alert(data[0]);
+                    
+                    
+                });
+            });
+          </script>
 </body>
 
 
