0e1231e000b63adb072c8507a53ce632d71b227f
wildan220 <wildanachmad73@gmail.com>
2022-08-02 11:03:14 +0700

    layouting mas mas feedback

--- resources/views/Feedback/index.blade.php
+++ resources/views/Feedback/index.blade.php
@@ -1,21 +1,40 @@
 <!doctype html>
-<html lang="en">
+<html lang="en" class="h-100">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Feedback</title>
-    <h1 class="mb-3 text-center">Feedback </h1>
+    
+    
+    <meta name="generator" content="Hugo 0.101.0">
+    <title>FEEDBACK</title>
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/cover/">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link href="assets/css/cover.css" rel="stylesheet">
   </head>
 
-<body>
-    <div class="form-step form-step-active">
-
-
-
+<body class="d-flex h-100 ">
+  
+  
+  <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <header class="mb-5">
+      <div>
+        <h3 class="float-md-start mb-0">FeedBack</h3>
+        <nav class="nav nav-masthead justify-content-center float-md-end">
+          
+        </nav>
+      </div>
+    </header>
+  
+    <main class="px-3">
+      <h1>Selamat Datang</h1>
+      <p class="lead">silahkan mengisi penilaian anda pada tombol feedback pada tabel sebelah kanan 
+                      kami mengucapkan terima kasih telah menggunakan layanan kami</p>
+      <p class="lead">
+      <div class="form-step form-step-active">
         <table class="table table-striped">
             <thead>
               <tr>
@@ -91,7 +110,7 @@
                           <label class="form-check-label" for="1">1</label>
                         </div>
                         <div class="form-check form-check-inline">
-                          <input class="form-check-input" type="radio" name="fasility" id="2" value="2">
+                          <input class="form-check-input" type="radio" name="facility" id="2" value="2">
                           <label class="form-check-label" for="2">2</label>
                         </div>
                         <div class="form-check form-check-inline">
@@ -171,6 +190,11 @@
 
 
           <!-- Button trigger modal -->
+    </main>
+  
+    
+  </div>
+  
 
 </body>
 
