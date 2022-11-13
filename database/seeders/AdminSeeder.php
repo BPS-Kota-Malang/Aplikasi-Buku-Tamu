aabbf5355b082e870f585c7e06fa6153a5b4a3a9
darman1725 <darmansaragih1701@gmail.com>
2022-11-13 20:10:55 +0700

    Mengisi data pada database dengan seed (Menu Admin)

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -24,24 +24,8 @@ class AdminSeeder extends Seeder
         ]);
 
         User::create([
-            'name'     => 'Darman Saragi',
+            'name'     => 'Darman Saragih',
             'email'    => 'darman123@gmail.com',
-            'role'     => 'Super Admin',
-            'password' => bcrypt('admin1234'),
-        ]);
-        User::create([
-            'name'     => 'Rara',
-            'email'    => 'rara22@gmail.com',
-            'role'     => 'Super Admin',
-            'password' => bcrypt('paskibra'),
-
-            'name'     => 'wildan',
-            'email'    => 'wildan123@gmail.com',
-            'role'     => 'Super Admin',
-            'password' => bcrypt('admin1234'),
-
-            'name'     => 'orin',
-            'email'    => 'orin123@gmail.com',
             'role'     => 'Super Admin',
             'password' => bcrypt('admin1234'),
         ]);
