c590ddbf5b3295229f7739f38c31265041b02bc4
darman1725 <darmansaragih1701@gmail.com>
2022-11-22 20:02:25 +0700

    Melengkapi resource dan clean code

--- resources/views/Feedback/show.blade.php
+++ resources/views/Feedback/show.blade.php
@@ -184,7 +184,6 @@
                     </div>
                   </div>
                 </div>
-
                 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
 
@@ -209,6 +208,7 @@
                 </script>
             </div>
         </div>
+        <a class="btn btn-dark fadeIn fourth" style="margin-left: 15px; padding: 15px 30px;" href="{{ url('feedback') }}">&xlarr; Kembali</a><br>
     </div>
   </div>
 </body>
